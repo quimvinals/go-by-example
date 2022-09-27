66c15e14d764c36f6ed74fd98a27daa5168daf6c
BalisticMan <quim.baget@estudiantat.upc.edu>
2022-09-27 18:59:40 +0200

    testing with custom function

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func sayHello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -2,11 +2,26 @@ package main
 
 import "testing"
 
-func TestSayHello(t *testing.T) {
-	got := sayHello("Quim")
-	expected := "Hello, Quim"
+func assertCorrectMessage(t testing.TB, got, expected string) {
+	t.Helper()
 
 	if got != expected {
 		t.Errorf("got %q but expected %q", got, expected)
 	}
+}
+
+func TestSayHello(t *testing.T) {
+	t.Run("saying hello to custom people", func(t *testing.T) {
+		got := sayHello("Quim")
+		expected := "Hello, Quim"
+
+		assertCorrectMessage(t, got, expected)
+	})
+
+	t.Run("say 'Hello, World' when no name provided", func(t *testing.T) {
+		got := sayHello("")
+		expected := "Hello, World"
+
+		assertCorrectMessage(t, got, expected)
+	})
 }
