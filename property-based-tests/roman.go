55dc78aa23d398cb7e9c4a84294096e8729f12e4
Quim Baget Salarich <quim.baget@paack.co>
2023-12-04 17:03:19 +0100

    Implement Roman parser with property based test

---
+++ property-based-tests/roman.go
@@ -0,0 +1,37 @@
+package propertyTests
+
+import "strings"
+
+type RomanParser struct {
+	arabic int
+	roman  string
+}
+
+var romanDictionary = []RomanParser{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for _, romanNumber := range romanDictionary {
+		for arabic >= romanNumber.arabic {
+			result.WriteString(romanNumber.roman)
+			arabic -= romanNumber.arabic
+		}
+	}
+
+	return result.String()
+}
